4be41d9204fe8e63d8f0649ea495c9ffb791aba1
美少女战士 <14347332+beautiul-girl-warrior@user.noreply.gitee.com>
2024-11-10 20:39:23 +0800

    Initial commit of the project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+// 导入React库
+import React, { useEffect } from 'react';
+// 导入React Router DOM相关组件
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// 导入自定义组件
+import TaskList from './components/TaskList';
+import TaskDetail from './components/TaskDetail';
+// 导入fetchTasks函数
+import { fetchTasks } from './redux/taskSlice';
+// 导入useDispatch hook
+import { useDispatch } from 'react-redux';
+// 导入ErrorBoundary组件
+import ErrorBoundary from './ErrorBoundary';
 
 function App() {
+  // 使用useDispatch hook创建dispatch函数
+  const dispatch = useDispatch();
+
+  // 使用useEffect hook，在组件挂载时执行fetchTasks函数
+  useEffect(() => {
+    dispatch(fetchTasks());
+  }, [dispatch]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // 渲染ErrorBoundary组件包裹整个应用
+    <ErrorBoundary>
+      {/* 使用Router组件 */}
+      <Router>
+        {/* 使用Routes组件 */}
+        <Routes>
+          {/* 定义根路径对应的组件 */}
+          <Route path="/" element={<TaskList />} />
+          {/* 定义带有动态参数/:id的路径对应的组件 */}
+          <Route path="/task/:id" element={<TaskDetail />} />
+        </Routes>
+      </Router>
+    </ErrorBoundary>
   );
 }
 
